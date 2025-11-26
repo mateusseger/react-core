5decc514a504fd13ce274bee3ec23125c9d59b8a
Mateus Seger Selau <mateus.selau@herval.com.br>
2025-11-26 08:06:22 -0300

    remove demo

--- demo/src/config/project.ts
+++
@@ -1,7 +0,0 @@
-// Configuração do projeto para a demo
-import type { ProjectConfig } from '@herval/react-core'
-
-export const projectConfig: ProjectConfig = {
-    name: 'React Core Demo',
-    version: '1.0.0',
-}
